8c0436cf9294a40e8da134d65825b73a843095ee
KevinBrandao <kevenbrandaoms@outlook.com>
2022-01-28 21:12:36 -0300

    sub

--- app/Http/Controllers/somaController.php
+++ app/Http/Controllers/somaController.php
@@ -91,7 +91,7 @@ class somaController extends Controller
     public function sub($num1, $num2)
     {
         $sub = $num1 - $num2;
-        logger()->debug(['Sub feita','num1' => $num1, 'num2' => $num2, 'sub' => $sub]); 
+        logger()->debug('Sub feita',['num1' => $num1, 'num2' => $num2, 'sub' => $sub]); 
         return ($sub);
     }
 }
